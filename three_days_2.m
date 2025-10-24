542e806d5157c3dadb203cf2d5c7b18d68b9b18d
I-Zanotti <ivanzanotti2002@gmail.com>
2025-10-24 15:43:23 +0200

    Add files via upload
    solo prova

---
+++ three_days_2.m
@@ -0,0 +1,51 @@
+clear, clc, close all
+
+[signal, Fs, tm] = rdsamp('data/CO001');
+
+g = 9.81;
+threshold_SMA = 0.135*g;
+threshold_en = 0.05;
+aV = signal(:,1);
+aML = signal(:,2);
+aAP = signal(:,3);
+
+window = Fs*1;
+p_overlap = 0.5;
+
+% limit = floor(length(aV)/window);
+limit = floor(length(aV)/(window*p_overlap)) - 1;
+SMA = zeros(limit,1);
+energy  = zeros(limit,1);
+for i = 0:(limit-1)
+    aVw = aV(window*i +1 : window*(i+1));
+    aMLw = aML(window*i +1 : window*(i+1));
+    aAPw = aAP(window*i +1 : window*(i+1));
+
+    SMA(i+1) = mean(abs(aVw) + abs(aMLw) + abs(aAPw));
+    [paAPw,f]=pwelch(aAPw, [], [], [], Fs); 
+    paAPw=paAPw(f>0.5 & f<3);
+    f=f(f>0.5 & f<3);
+
+    energy(i+1) = trapz(f,paAPw);
+end
+
+walking = (SMA > threshold_SMA) | (energy > threshold_en);
+%%
+
+i = 1;
+while i < length(walking)
+    if all(walking(i: min([i+59, length(walking)])))
+        i = i+1;
+        while all(walking(i: min([i+60, length(walking)])))
+            i = i+1;
+        end
+        i = i+59;
+    else
+        walking(i) = 0;
+    end
+    i = i+1;
+end
+
+sum(walking>0)/length(walking) * 100
+plot(walking)
+
