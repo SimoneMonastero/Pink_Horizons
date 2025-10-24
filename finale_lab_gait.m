2708e0804ce5511a20a993791c455848c2dd8efe
I-Zanotti <ivanzanotti2002@gmail.com>
2025-10-24 10:44:02 +0200

    Add files via upload

---
+++ finale_lab_gait.m
@@ -0,0 +1,109 @@
+clear, clc, close all
+
+C=load('LabCOData.mat');
+F=load('LabFLData.mat');
+Fs=100;
+
+namesC=fieldnames(C.allSubjectData);
+namesF=fieldnames(F.allSubjectData);
+lengthC=numel(namesC);
+lengthF=numel(namesF);
+gait_speed_co= zeros(lengthC, 1); % Initialize gait_speed array
+gait_speed_fl= zeros(lengthF, 1); 
+hsens=0.53; %da tabella antropometrica la lunghezza suolo-lombari è 0.53H
+height_con=1.64; %+-0.06 altezza nonfallers
+height_fall=1.61; %+-0.09 altezza fallers
+
+% bandpass filter creation
+lowCut = 0.5; % Hz
+highCut = 15; % Hz
+order = 4;
+Wn = [lowCut highCut] / (Fs/2);
+[B_bp, A_bp] = butter(order, Wn, 'bandpass');
+
+% bandpass filter creation for position
+lowCut = 0.5; % Hz
+highCut = 5; % Hz
+order = 4;
+Wn = [lowCut highCut] / (Fs/2);
+[B_pos, A_pos] = butter(order, Wn, 'bandpass');
+
+for i=1:lengthC
+    currentsubj=namesC{i};
+    accData=C.allSubjectData.(currentsubj).data(:,1:3);
+    tm=C.allSubjectData.(currentsubj).time;
+   [aAP, aML, aV] = algo_Moe_Nilssen(accData(:,3), accData(:,2), accData(:,1) ,'tiltAndNoG');
+    % Conversion from g to m/s^2:
+    g = 9.81;
+    aAP = aAP*g;
+    aML = aML*g;
+    aV = aV*g;
+    
+    [paAP,f]=pwelch(aAP, [], [], [], Fs);    % Pwelch vuole anche la sampling frequency, se no di default è 1Hz
+    paAP=paAP(f>0.5 & f<3);
+    f=f(f>0.5 & f<3);
+    [ampl, ind]=max(paAP); % amplitude psd che è passi/secondo
+    passi_sec=f(ind); %passi al secondo
+    
+    % filtering aV 
+    aV = filtfilt(B_bp, A_bp, aV); 
+    % velocity with cumtrapz
+    velocity=cumtrapz(tm,aV);
+    % filtering velocity 
+    velocity=filtfilt(B_bp,A_bp,velocity);
+    % position with cumtrapz
+    position=cumtrapz(tm, velocity);
+    % filtering position
+    position = filtfilt(B_pos, A_pos, position); 
+   
+    h=max(position)-min(position); % h è il cambiamento dell'altezza del COM
+    l_con = hsens* height_con; % altezza sensore nonfallers
+    
+    step_length=2*sqrt(2*l_con*h - h^2); % metri/passo
+    gait_speed_co(i)=step_length * passi_sec;
+end
+%plot(position)
+gait_speed_co=gait_speed_co';
+MCO=mean(gait_speed_co);
+SCO=std(gait_speed_co);
+fprintf("gait speed controls: %f +- %f \n",MCO,SCO)
+
+% now fallers
+
+for i=1:lengthF
+    currentsubj=namesF{i};
+    accData=F.allSubjectData.(currentsubj).data(:,1:3);
+    tm=F.allSubjectData.(currentsubj).time;
+   [aAP, aML, aV] = algo_Moe_Nilssen(accData(:,3), accData(:,2), accData(:,1) ,'tiltAndNoG');
+    % Conversion from g to m/s^2:
+    aAP = aAP*g;    aML = aML*g;    aV = aV*g;
+    
+    [paAP,f]=pwelch(aAP, [], [], [], Fs);    % Pwelch vuole anche la sampling frequency, se no di default è 1Hz
+    paAP=paAP(f>0.5 & f<3);
+    f=f(f>0.5 & f<3);
+    [ampl, ind]=max(paAP); % amplitude psd che è passi/secondo
+    passi_sec=f(ind); %passi al secondo
+    % bandpass filter already created
+   
+    % filtering aV 
+    aV = filtfilt(B_bp, A_bp, aV); 
+    % velocity with cumtrapz
+    velocity=cumtrapz(tm,aV);
+    % filtering velocity 
+    velocity=filtfilt(B_bp,A_bp,velocity);
+    % position with cumtrapz
+    position=cumtrapz(tm, velocity);
+    % filtering position
+    position = filtfilt(B_pos, A_pos, position); 
+   
+    h=max(position)-min(position); % h è il cambiamento dell'altezza del COM
+    l_fall = hsens * height_fall; % altezza sensore fallers
+    
+    step_length=2*sqrt(2*l_fall*h - h^2); % metri/passo
+    gait_speed_fl(i)=step_length * passi_sec;
+end
+plot(position)
+gait_speed_fl=gait_speed_fl';
+MFL=mean(gait_speed_fl);
+SFL=std(gait_speed_fl);
+fprintf("gait speed fallers: %f +- %f \n",MFL,SFL)
