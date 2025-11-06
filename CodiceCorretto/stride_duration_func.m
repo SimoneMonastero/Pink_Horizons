8869a5d2c95a0415fbd2efba44b71b230ac6a1f1
SManuzzi <samuele.manuzzi@studio.unibo.it>
2025-11-06 16:34:49 +0100

    Update stride_duration_func.m

--- CodiceCorretto/stride_duration_func.m
+++ CodiceCorretto/stride_duration_func.m
@@ -3,4 +3,6 @@ function  [stride_duration]=stride_duration_func(FSs)
     % Calculate the stride duration based on the sampling frequency
     step_period=1/FSs;
     stride_duration = 2*step_period; 
+    fprintf('stride duration = %d \n',stride_duration)
+
 end
