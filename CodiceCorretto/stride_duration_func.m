f8ba5eba2325f692012347258b5582c3ce30b18e
sofiamorettiii <sofia_moretti@icloud.com>
2025-11-12 12:07:27 +0100

    Refine comments in stride_duration_func
    Updated function comments for clarity.

--- CodiceCorretto/stride_duration_func.m
+++ CodiceCorretto/stride_duration_func.m
@@ -1,8 +1,9 @@
 function  [stride_duration]=stride_duration_func(FSs)
-    %    definition of the function
-    % Calculate the stride duration based on the sampling frequency
-    step_period=1/FSs;
-    stride_duration = 2*step_period; 
+   % Function to calculate stride duration from step frequency
+   
+    step_period=1/FSs;  
+    stride_duration = 2*step_period;  
     fprintf('stride duration = %d \n',stride_duration)
 
 end
+
